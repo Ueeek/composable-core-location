455e5c9b31210c1dd427f2a745f2822dcba30b81
Ueeek <uekikoichiro@gmail.com>
2024-12-04 21:55:36 +0900

    add package dependency

--- Package@swift-6.0.swift
+++ Package@swift-6.0.swift
@@ -38,10 +38,10 @@ let package = Package(
 
 for target in package.targets {
   target.swiftSettings = target.swiftSettings ?? []
-  target.swiftSettings?.append(
+  //target.swiftSettings?.append(
     //.unsafeFlags([
       //"-Xfrontend", "-warn-concurrency",
       //"-Xfrontend", "-enable-actor-data-race-checks",
     //])
-  )
+  //)
 }
